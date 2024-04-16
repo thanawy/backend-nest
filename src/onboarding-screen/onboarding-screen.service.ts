9174da52091585c821eaa0fdf6d2bab010302077
Abdelrahman Saleh <abdelrahman.noureldin.a@gmail.com>
2024-04-17 01:58:29 +0200

    change relative import to path imports via @

--- src/onboarding-screen/onboarding-screen.service.ts
+++ src/onboarding-screen/onboarding-screen.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { OnboardingScreen } from '@onboarding-screen/entities/onboarding-screen.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { CreateOnboardingScreenDto } from './dto/create.onboarding-screen.dto';
-import { UpdateOnboardingScreenDto } from './dto/update.onboarding-screen.dto';
+import { CreateOnboardingScreenDto } from '@onboarding-screen/dto/create.onboarding-screen.dto';
+import { UpdateOnboardingScreenDto } from '@onboarding-screen/dto/update.onboarding-screen.dto';
 @Injectable()
 export class OnboardingScreenService {
 

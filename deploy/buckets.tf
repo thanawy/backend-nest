26aea12eb43f586727740cb2547a9fb8719aee63
Abdelrahman Noureldin <abdelrahman.noureldin.a@gmail.com>
2024-05-12 21:59:07 +0200

    THAN-30 :: final test hopefully x2

--- deploy/buckets.tf
+++ deploy/buckets.tf
@@ -9,5 +9,10 @@ resource "google_storage_bucket" "terraform_state_bucket" {
   depends_on = [
     google_project_service.gcp_services
   ]
+}
 
+resource "google_storage_bucket_iam_member" "bucket_object_admin" {
+  bucket = google_storage_bucket.terraform_state_bucket.name
+  role   = "roles/storage.objectAdmin"
+  member = "serviceAccount:${google_service_account.github_actions.email}"
 }
